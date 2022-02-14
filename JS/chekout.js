1ca8cbbca0bcc3315aa2bf39f815a697254c49f9
zarayach <mohamedachbani5@gmail.com>
2022-02-14 00:04:05 +0000

    some improvement

---
+++ JS/chekout.js
@@ -0,0 +1,122 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FOR THE CARD EXPIRATION
+
+const monthInput = document.querySelector('#month');
+const yearInput = document.querySelector('#year');
+
+const focusSibling = function(target, direction, callback) {
+  const nextTarget = target[direction];
+  nextTarget && nextTarget.focus();
+  // if callback is supplied we return the sibling target which has focus
+  callback && callback(nextTarget);
+}
+
+// input event only fires if there is space in the input for entry. 
+// If an input of x length has x characters, keyboard press will not fire this input event.
+monthInput.addEventListener('input', (event) => {
+
+  const value = event.target.value.toString();
+  // adds 0 to month user input like 9 -> 09
+  if (value.length === 1 && value > 1) {
+      event.target.value = "0" + value;
+  }
+  // bounds
+  if (value === "00") {
+      event.target.value = "01";
+  } else if (value > 12) {
+      event.target.value = "12";
+  }
+  // if we have a filled input we jump to the year input
+  2 <= event.target.value.length && focusSibling(event.target, "nextElementSibling");
+  event.stopImmediatePropagation();
+});
+
+yearInput.addEventListener('keydown', (event) => {
+  // if the year is empty jump to the month input
+  if (event.key === "Backspace" && event.target.selectionStart === 0) {
+    focusSibling(event.target, "previousElementSibling");
+    event.stopImmediatePropagation();
+  }
+});
+
+const inputMatchesPattern = function(e) {
+  const { 
+    value, 
+    selectionStart, 
+    selectionEnd, 
+    pattern 
+  } = e.target;
+  
+  const character = String.fromCharCode(e.which);
+  const proposedEntry = value.slice(0, selectionStart) + character + value.slice(selectionEnd);
+  const match = proposedEntry.match(pattern);
+  
+  return e.metaKey || // cmd/ctrl
+    e.which <= 0 || // arrow keys
+    e.which == 8 || // delete key
+    match && match["0"] === match.input; // pattern regex isMatch - workaround for passing [0-9]* into RegExp
+};
+
+document.querySelectorAll('input[data-pattern-validate]').forEach(el => el.addEventListener('keypress', e => {
+  if (!inputMatchesPattern(e)) {
+    return e.preventDefault();
+  }
+}));
+// END CARD EXPERTION
--- chekout.php
+++ chekout.php
@@ -1,10 +1,132 @@
 <?php
 if(isset($_COOKIE['logined'])){
     if($_COOKIE['logined']){
-        echo 'yeah';
+        // echo 'yeah';
     }else{
-        echo 'IT FALSE';
+        // echo 'IT FALSE';
     }
 }else{
-    echo 'NO';
+    // echo 'NO';
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/chekout.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script src="https://kit.fontawesome.com/8b4d616e93.js" crossorigin="anonymous"></script>
+    <title>Chekout</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left">
+            <div class="back">
+                    <a href="#"><i class="fa fa-arrow-left" aria-hidden="true"></i>
+                    </a>
+                   <p>Continu exploring</p> 
+                </div>
+            <div class="head" > 
+                <div class="title">
+                 <h1>waiting your reservation! </h1>
+                 <span>you have <!--number from database -->1 trips in your list</span>
+                </div>      
+                <div class="sort">
+                    <p>Sort By :</p>
+                    <select name="" id="">
+                        <option value="price">Price</option>
+                        <option value="data">Date</option>
+                    </select>
+
+                </div> 
+                </div>
+                
+               <div class="cards_container">
+               <div class="card">
+                    <div class="img"><img src="./IMG/835a014c3ce140d01c1d2b4e4b913487.jpg" alt="" srcset=""></div>
+                    <div class="card_title">
+                        <div class="trip_title">TO marakeck</div>
+                        <div class="trip_destination">Lorem, ipsum dolor.</div>
+                    </div>
+                    <div class="Number">
+                        <div id="plus"><i class="fa fa-plus-circle" aria-hidden="true"></i></div>
+                        <div class="person">1</div>
+                        <div id="minus"><i class="fa fa-minus-circle" aria-hidden="true"></i></div>
+                    </div>
+                    <div class="price"><span id="currency">$</span>999.99</div>
+                    <div class="delete"><i class="fa-solid fa-trash-can"></i></div>
+                </div> 
+
+
+                
+               </div>
+        
+        </div>
+        <div class="right">
+            <div class="right_head">
+                <div class="right_title">card details</div>
+                <div class="UserImg"><img src="./IMG/pic1.png" alt=""></div>
+            </div>
+            <div class="ways">
+            <p>card type :</p>
+               <div class="cards">
+                   <div id="pay">
+                    <i  class="fa-brands fa-cc-mastercard"></i>
+                    <span id="selected"><i class="material-icons">check_circle</i></span>
+
+                </div>
+                <div class="active" id="pay" >
+                <i class="fa-brands fa-cc-visa"></i>
+                    <span class="active" id="selected"><i class="material-icons">check_circle</i></span>
+                   
+                </div>
+                <div id="pay">
+                <i class="fa-brands fa-cc-paypal"></i>
+                    <span id="selected"><i class="material-icons">check_circle</i></span>
+                  
+                </div>
+               </div>
+                </div>
+                <div class="form">
+                    <label for="holder-name">card holder name</label>
+                    <input id="holder-name" type="text" placeholder="Tom Hanks">
+                    <label for="card-number">Card Number</label>
+                    <input id="card-number" type="text" inputmode="numeric" placeholder="1111 2222 3333 4444">
+                    <div class="para">
+                    <label for="experation-date">Experation Date</label>
+                    <div class="exp-wrapper">
+                            <input autocomplete="off" class="exp" id="month" maxlength="2" pattern="[0-9]*" inputmode="numerical" placeholder="MM" type="text" data-pattern-validate />
+                            <input autocomplete="off" class="exp" id="year" maxlength="2" pattern="[0-9]*" inputmode="numerical" placeholder="YY" type="text" data-pattern-validate />
+                    </div>
+                    <label for="cvv">cvv</label>
+                    <input id="cvv" maxlength="3" inputmode="numeric" type="text" placeholder="123" >
+                    </div>
+                </div>
+                <div class="summary">
+                    <div class="subTotal">
+                        <p>Sub Total :</p>
+                        <span>TOTAL</span>
+                    </div>
+                    <div class="extra">
+                        <p>Extra charge :</p>
+                        <span>TOTAL</span>
+                    </div>
+                    <div class="Total">
+                        <p>Total :</p>
+                        <span id="total" >TOTAL</span>
+                    </div>
+                </div>
+                <div class="validate"><a href="" id="chekout-btn"><span id="total">total</span>
+                <span>chekout</span>
+                <i class="fa fa-arrow-right" aria-hidden="true"></i>
+            </a>
+        </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="JS\chekout.js"></script>
+</body>
+</html>
